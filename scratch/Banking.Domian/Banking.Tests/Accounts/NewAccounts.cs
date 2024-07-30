bb509f0f4b9ecbd2c68f35321d4804709fa60530
SaragamGadal <saragam.g123@gmail.com>
2024-07-30 12:34:14 -0400

    before lunch

---
+++ scratch/Banking.Domian/Banking.Tests/Accounts/NewAccounts.cs
@@ -0,0 +1,17 @@
+﻿using Banking.Domain;
+
+namespace Banking.Tests.Accounts;
+public class NewAccounts
+{
+    [Fact]
+    public void NewAccountsHaveCorrectOpeningBalance()
+    {
+        // Given I have a brand new account
+        Account account = new Account();
+        // When I ask that account for the balance
+        decimal currentBalance = account.GetBalance();
+        // Then it should be the correct opening balance
+        Assert.Equal(5000, currentBalance);
+
+    }
+}
